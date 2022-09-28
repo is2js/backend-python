81f9033130ac7869d68e19bc9e1a1e39dfaa1c89
is2js <tingstyle1@gmail.com>
2022-09-28 16:13:40 +0900

    feat: implementing select_user in UserRepository

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,7 +1,9 @@
 # pylint: disable=E1101
-from collections import namedtuple
+
+from typing import List
+from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
+from src.infra.entities import Users as UsersModel
 
 
 class UserRepository:
@@ -15,15 +17,17 @@ class UserRepository:
         :return - tuple with new user inserted
         """
 
-        InsertData = namedtuple("Users", "id name password")
+        # InsertData = namedtuple("Users", "id name password")
 
         with DBConnectionHandler() as db_connection:
             try:
-                new_uesr = Users(name=name, password=password)
+                # new_uesr = Users(name=name, password=password)
+                new_uesr = UsersModel(name=name, password=password)
                 db_connection.session.add(new_uesr)
                 db_connection.session.commit()
 
-                return InsertData(
+                # return InsertData(
+                return Users(
                     id=new_uesr.id, name=new_uesr.name, password=new_uesr.password
                 )
             except:
@@ -33,3 +37,49 @@ class UserRepository:
                 db_connection.session.close()
 
         return None
+
+    @classmethod
+    def select_user(cls, user_id: int = None, name: str = None) -> List[Users]:
+        """
+        Select data in user entity by id and/or name
+        :param - user_id: Id of the registry
+               - name: User name
+        :return - List with Users selected
+        """
+        try:
+            query_data = None
+
+            if user_id and not name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif not user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UsersModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            return query_data
+
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
