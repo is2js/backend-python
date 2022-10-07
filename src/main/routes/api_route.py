a0e06350e26bf473884117a46fc3dc0664ba6f0a
is2js <tingstyle1@gmail.com>
2022-10-08 01:02:03 +0900

    improve: enabling debugger

--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,12 +1,43 @@
-from flask import Blueprint, jsonify
-
-# from src.main.composer import register_user_composer
+from flask import Blueprint, jsonify, request
+from src.main.adapter import flask_adapter
+from src.main.composer import register_user_composer
 
 api_routes_bp = Blueprint("api_routes", __name__)
 
 
-@api_routes_bp.route("/api", methods=["GET"])
+# @api_routes_bp.route("/api", methods=["GET"])
+@api_routes_bp.route("/api", methods=["POST"])
 def something():
     """test"""
 
-    return jsonify({"Programmer": "Dolbum"})
+    # return jsonify({"message": request.args.to_dict()})
+    return jsonify({"message": request.json})
+
+
+@api_routes_bp.route("/api/users", methods=["POST"])
+def register_user():
+    """register user route"""
+
+    message = {}
+    response = flask_adapter(request=request, api_route=register_user_composer())
+
+    if response.status_code < 300:
+        message = {
+            "Type": "users",
+            "id": response.body.id,
+            "attributes": {"name": response.body.name},
+        }
+        return jsonify({"data": message}), response.status_code
+
+    # Handing Errors
+    return (
+        jsonify(
+            {
+                "error": {
+                    "status": response.status_code,
+                    "title": response.body["error"],
+                }
+            }
+        ),
+        response.status_code,
+    )
