7a3d328d21cc6830d50fae2d688cee631d7f9522
is2js <tingstyle1@gmail.com>
2022-10-08 22:44:55 +0900

    feat: implementing register_pet Route

---
+++ src/main/composer/register_pet_composite.py
@@ -0,0 +1,21 @@
+from src.infra.repo.pet_repository import PetRepository
+from src.infra.repo.user_repository import UserRepository
+from src.data.register_pet import RegisterPet
+from src.data.find_user import FindUser
+from src.main.interface.route import RouteInterface
+from src.presenters.controllers import RegisterPetController
+
+
+def register_pet_composer() -> RouteInterface:
+    """Composing Register Pet Route
+    :param - None
+    :return - Object with Register Pet Route
+    """
+
+    pet_repository = PetRepository()
+    find_user_use_case = FindUser(UserRepository())
+    use_case = RegisterPet(pet_repository, find_user_use_case)
+
+    register_pet_route = RegisterPetController(use_case)
+
+    return register_pet_route
--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, jsonify, request
 from src.main.adapter import flask_adapter
-from src.main.composer import register_user_composer
+from src.main.composer import register_user_composer, register_pet_composer
 
 api_routes_bp = Blueprint("api_routes", __name__)
 
 
 # @api_routes_bp.route("/api", methods=["GET"])
-@api_routes_bp.route("/api", methods=["POST"])
-def something():
-    """test"""
+# @api_routes_bp.route("/api", methods=["POST"])
+# def something():
+# """test"""
 
-    # return jsonify({"message": request.args.to_dict()})
-    return jsonify({"message": request.json})
+# return jsonify({"message": request.args.to_dict()})
+# return jsonify({"message": request.json})
 
 
 @api_routes_bp.route("/api/users", methods=["POST"])
@@ -23,9 +23,43 @@ def register_user():
 
     if response.status_code < 300:
         message = {
-            "Type": "users",
+            "type": "users",
             "id": response.body.id,
             "attributes": {"name": response.body.name},
+        }
+        return jsonify({"data": message}), response.status_code
+
+    # Handing Errors
+    return (
+        jsonify(
+            {
+                "error": {
+                    "status": response.status_code,
+                    "title": response.body["error"],
+                }
+            }
+        ),
+        response.status_code,
+    )
+
+
+@api_routes_bp.route("/api/pets", methods=["POST"])
+def register_pet():
+    """register pet route"""
+
+    message = {}
+    response = flask_adapter(request=request, api_route=register_pet_composer())
+
+    if response.status_code < 300:
+        message = {
+            "type": "pets",
+            "id": response.body.id,
+            "attributes": {
+                "name": response.body.name,
+                "specie": response.body.specie,
+                "age": response.body.age,
+            },
+            "relationships": {"owner": {"type": "users", "id": response.body.user_id}},
         }
         return jsonify({"data": message}), response.status_code
 
